dd92b25306ed9c1a395d0dd61d7f120caa802310
Kyle McWilliams <kmac52694@gmail.com>
2021-08-06 17:49:51 -0500

    recommiting everything

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+
+let form = document.getElementById('passwordGeneratingForm')
+let upperCaseElement = document.getElementById('upperCase')  
+let lowerCaseElement = document.getElementById('lowerCase')  
+let numbersElement = document.getElementById('numbers')
+let specialCharElement = document.getElementById('specialSymbol')
+let characterAmountElement = document.getElementById('characterAmount')
+
+let upperCaseArray = (Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+let lowerCaseArray = (Array.from('abcdefghjiklmnopqrstuvwxyz'));
+let numbersArray = (Array.from('1234567890'));
+let specialCharArray = (Array.from('!@#$%^&*()|~`-=+'));
+
+var passwordLength = characterAmountElement.value;
+var upperCaseResponse = upperCaseElement.value;
+var lowerCaseResponse = lowerCaseElement.value;
+var numberResponse = numbersElement.value;
+var specialCharResponse = specialCharElement.value;
+var merge = [];
+
+// what happens when the generate button is clicked.
+form.addEventListener("submit" , function(event) {
+        event.preventDefault();
+        const values = Array.from(document.querySelectorAll("input[type = checkbox]:checked"))
+        merge = [];
+        const numberOfChars = Array.from(document.querySelectorAll("input[type = number]"))
+        console.log(numberOfChars);
+        // checks to see if each button is clicked or not.
+        console.log(values[0]);
+
+//selecting which values to add to the loop
+ values.map(function (value, i) {
+     console.log(value.id);
+     if(value.id === "upperCase"){
+     merge = [...merge , ...upperCaseArray]
+     console.log(merge);
+     }
+     if(value.id === "lowerCase"){
+        merge = [...merge, ...lowerCaseArray]
+        console.log(merge);
+     }
+     if(value.id === "numbers"){
+        merge = [...merge , ...numbersArray]
+        console.log(merge);
+     }
+     if(value.id === "specialSymbol"){
+        merge = [...merge , ...specialCharArray]
+        console.log(merge);
+     }
+
+ for (let i = 0; i < characterAmountElement.value; i++) {
+   console.log (Math.floor(Math.random() * merge.length));
+ }
+
+
+ })
+
+        // get password length to loop through password length
+})
